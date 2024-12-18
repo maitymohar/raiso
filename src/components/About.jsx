cf2cfc5f0b2aec1cd65e8a851dcead072e0dbf65
maitymohar <maitymohar2005@gmail.com>
2024-12-18 12:32:59 +0530

    Animated Title completion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,63 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle";
 
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
   return (
     <div id="about" className="min-h-screen w-screen">
-        <div className="realtice mb-8 mt-36 flex flex-col items-center gap-5">
-            <h2 className="font-general text-sm uppercase  md:text-[10px]">Welcome to LCS</h2>
-            <div className="mt-5 text-center text-4xl uppercase leading-0.8 md:text-[6rem]">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            </div>
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </p>
 
-            <div className="about-subtext">
-                <p>Lorem ipsum dolor sit.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            </div>
+        <AnimatedTitle
+         title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+         containerClass="mt-4 text-4xl !test-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, laboriosam animi!</p>
+          <p className="text-gray-500">
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ad, hic rem natus vitae odio error laboriosam corporis!
+          </p>
         </div>
-        <div className="h-dvh w-screen" id="clip">
-            <div className="mask-clip-path about-image">
-                <img src="img/about.webg" alt="Background"
-                className="absolute left-0 top-0 size-full object-cover" />
-            </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
